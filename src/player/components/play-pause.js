7c19b7f4e9571d345ba444766550c3ee705d589c
Katherine Aguilera <katherinam35@gmail.com>
2018-11-27 16:58:26 -0500

    Botones de Play y Pause

---
+++ src/player/components/play-pause.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './play-pause.css';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+const PlayPause = props=>(
+    <div className="PlayPause">
+    {/* {}if ternario */}
+      {
+        // props.pause===true ? si se cumple boton play
+        props.pause ?
+          <button
+            onClick={props.handleClick}
+          >
+            <Play size={25} color="white" />
+          </button>
+          // : no se cumple boton pause
+        :
+        <button
+          onClick={props.handleClick}
+        >
+          <Pause size={25} color="white" />
+        </button>
+      }
+    </div>
+  )
+ export default PlayPause
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
+
  class VideoPlayer extends Component {
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => {
+    this.setState({
+      // pause sera lo contario a lo definido
+      pause: !this.state.pause
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
           titulo="Esto es un video chido!"
+        />
+        <PlayPause
+        // mostrar dependiendo del estado play/pause
+          pause={this.state.pause}
+          // handleClick de play-pause (props.handleclick)
+          //togglePlay sera una funcion
+          handleClick={this.togglePlay}
         />
         <Video
           autoplay={true}
