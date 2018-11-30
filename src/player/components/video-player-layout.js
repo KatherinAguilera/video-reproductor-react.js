11c715fc7bc22a644806268646d0f9d63eb022cc
Katherine Aguilera <katherinam35@gmail.com>
2018-11-29 21:01:09 -0500

    Fullscreen

--- src/player/components/video-player-layout.js
+++ src/player/components/video-player-layout.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './video-player-layout.css';
- const VideoPlayerLayout = (props) => (
-  <div className="VideoPlayer">
-   {/* redenderizar */}
+import React from "react";
+import "./video-player-layout.css";
+const VideoPlayerLayout = props => (
+  <div
+    className="VideoPlayer"
+    // hacer referencia para que aparezca todos los elemento en el fullscreen
+    ref={props.setRef}
+  >
+    {" "}
     {props.children}
   </div>
-)
- export default VideoPlayerLayout
+);
+export default VideoPlayerLayout;
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
-import VideoPlayerLayout from '../components/video-player-layout';
-import Video from '../components/video';
-import Title from '../components/title';
-import PlayPause from '../components/play-pause';
-import Timer from '../components/timer.js';
-import Controls from '../components/video-player-controls.js';
-import formattedTime from'../../util/timer'
-import ProgressBar from '../components/progress-bar';
-import Spinner from '../components/spinner';
-import Volume from '../components/volume';
+import React, {
+  Component
+} from "react";
+import VideoPlayerLayout from "../components/video-player-layout";
+import Video from "../components/video";
+import Title from "../components/title";
+import PlayPause from "../components/play-pause";
+import Timer from "../components/timer.js";
+import Controls from "../components/video-player-controls.js";
+import formattedTime from "../../util/timer";
+import ProgressBar from "../components/progress-bar";
+import Spinner from "../components/spinner";
+import Volume from "../components/volume";
+import FullScreen from "../components/full-screen";
 
- class VideoPlayer extends Component {
+class VideoPlayer extends Component {
   state = {
     pause: true,
     duration: 0,
     currentTime: 0,
     loading: false,
-    onOff: 1,
-  }
-  togglePlay = (event) => {
+    onOff: 1
+  };
+  togglePlay = event => {
     this.setState({
       // pause sera lo contario a lo definido
       pause: !this.state.pause
-    })
-  }
+    });
+  };
   // comienza a cargar
   handleSeeking = event => {
     this.setState({
       loading: true
-    })
-  }
+    });
+  };
   // deja de cargar
   handleSeeked = event => {
     this.setState({
       loading: false
-    })
-  }
+    });
+  };
   // componente montado validar play/pause
   componentDidMount() {
     this.setState({
       // si se cumple es true sino es false
-      pause: (!this.props.autoplay)
-    })
+      pause: !this.props.autoplay
+    });
   }
   handleLoadedMetadata = event => {
     // quien dispara el evento
@@ -49,78 +52,134 @@ import Volume from '../components/volume';
     this.setState({
       duration: this.video.duration
     });
-  }
+  };
   // cuando de actualice el tiempo que hacemos
   handleTimeUpdate = event => {
     // console.log(this.video.currentTime)
     this.setState({
       // currentTime es propiedad de html con su API de media
       currentTime: this.video.currentTime
-    })
-  }
+    });
+  };
   handleProgressChange = event => {
     // event.target.value
-    this.video.currentTime = event.target.value
-  }
+    this.video.currentTime = event.target.value;
+  };
   handleVolumeChange = event => {
     //volume elemento nativo del navegador de video
     this.video.volume = event.target.value;
     this.setState({
-			onOff: this.video.volume
-		})
-  }
+      onOff: this.video.volume
+    });
+  };
+  // Quitar volume mute
   handleVolClick = event => {
-		this.video.volume = !this.video.volume
-		this.setState({
-			onOff: this.video.volume
-		})
-	}
+    this.video.volume = !this.video.volume;
+    this.setState({
+      onOff: this.video.volume
+    });
+  };
+  handleFullScreenClick = event => {
+    // SALGO DE FULL screenen mozila
+    if (document.mozFullScreen) {
+      document.mozCancelFullScreen();
+    }
+    // mando a full screen
+    else if (this.player.mozRequestFullScreen) {
+      this.player.mozRequestFullScreen();
+    }
+    if (document.webkitIsFullScreen) {
+      document.webkitExitFullscreen();
+    }
+    else if (this.player.webkitRequestFullscreen) {
+      this.player.webkitRequestFullscreen();
+    }
+  };
+  setRef = element => {
+    this.player = element;
+  };
   render() {
     return (
-      <VideoPlayerLayout>
-        <Title
-          titulo="Esto es un video chido!"
+      // hacer referencia al elemento
+      <VideoPlayerLayout setRef = {
+          this.setRef
+        }
+      >
+      <Title titulo = "Esto es un video chido!" / >
+      <Controls >
+        <PlayPause
+        // mostrar dependiendo del estado play/pause
+        pause = {
+          this.state.pause
+        }
+        // handleClick de play-pause (props.handleclick)
+        //togglePlay sera una funcion
+        handleClick = {
+          this.togglePlay
+        }
+        /> 
+        <Timer duration = {
+          formattedTime(this.state.duration)
+        }
+        // pasamos el estado - luego a time.js
+        currentTime = {
+          formattedTime(this.state.currentTime)
+        }
         />
-        <Controls>
-          <PlayPause
-            // mostrar dependiendo del estado play/pause
-            pause={this.state.pause}
-            // handleClick de play-pause (props.handleclick)
-            //togglePlay sera una funcion
-            handleClick={this.togglePlay}
-          />
-          <Timer
-            duration={formattedTime(this.state.duration)}
-            // pasamos el estado - luego a time.js
-            currentTime={formattedTime(this.state.currentTime)}
-          />
-           <ProgressBar
-            duration={this.state.duration}
-            value={this.state.currentTime}
-            handleProgressChange={this.handleProgressChange}
-          />
-           <Volume
-            onOff={this.state.onOff}
-            handleVolClick={this.handleVolClick}
-            handleVolumeChange={this.handleVolumeChange}
-          />
-        </Controls>
-        <Spinner
-        // validar estado loading si esta acyivo se pone
-        active={this.state.loading}
+        <ProgressBar duration = {
+          this.state.duration
+        }
+        value = {
+          this.state.currentTime
+        }
+        handleProgressChange = {
+          this.handleProgressChange
+        }
         />
-        <Video
-          autoplay={this.props.autoplay}
-          // this.state.pause pasando estado de play/pause
-          pause={this.state.pause}
-          handleLoadedMetadata={this.handleLoadedMetadata}
-          handleTimeUpdate={this.handleTimeUpdate}
-          handleSeeking={this.handleSeeking}
-          handleSeeked={this.handleSeeked}
-          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        <Volume onOff = {
+          this.state.onOff
+        }
+        handleVolClick = {
+          this.handleVolClick
+        }
+        handleVolumeChange = {
+          this.handleVolumeChange
+        }
         />
+        <FullScreen handleFullScreenClick = {
+          this.handleFullScreenClick
+        }
+        /> 
+      </Controls>
+      <Spinner
+      // validar estado loading si esta acyivo se pone
+      active = {
+        this.state.loading
+      }
+      />
+      <Video autoplay = {
+        this.props.autoplay
+      }
+      // this.state.pause pasando estado de play/pause
+      pause = {
+        this.state.pause
+      }
+      handleLoadedMetadata = {
+        this.handleLoadedMetadata
+      }
+      handleTimeUpdate = {
+        this.handleTimeUpdate
+      }
+      handleSeeking = {
+        this.handleSeeking
+      }
+      handleSeeked = {
+        this.handleSeeked
+      }
+      src = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" /
+      >
       </VideoPlayerLayout>
-    )
+    );
   }
 }
- export default VideoPlayer;
+export default VideoPlayer;
